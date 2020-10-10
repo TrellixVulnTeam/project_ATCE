7e8f154e66673da306848d28b5e9ca6213f08d71
Sergey Ermakov <oino-dev@ya.ru>
2020-10-10 22:43:42 +0300

    update

---
+++ to-do-list-advanced/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+//Importing Components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+	const [ inputText, setInputText ] = useState('');
+	const [ todos, setTodos ] = useState([]);
+	return (
+		<div className="App">
+			<header>
+				<h1>Список задач </h1>
+			</header>
+			<Form setInputText={setInputText} />
+			<TodoList />
+		</div>
+	);
+}
+
+export default App;
