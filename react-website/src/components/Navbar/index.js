c3aefb0a26a3128f4d74c59b598bd99d69fd7a19
Sergey Ermakov <oino-dev@ya.ru>
2020-10-15 11:40:06 +0300

    Update

---
+++ react-website/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Nav, NavbarContainer, NavLogo } from './NavbarElemets';
+
+const Navbar = () => {
+	return (
+		<NavbarContainer>
+			<NavLogo to="/">Example</NavLogo>
+		</NavbarContainer>
+	);
+};
+
+export default Navbar;
