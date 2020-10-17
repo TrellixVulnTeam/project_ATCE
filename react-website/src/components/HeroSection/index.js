43c404da807677f9ed90508cfa27200c0d9abcf4
Sergey Ermakov <oino-dev@ya.ru>
2020-10-18 00:00:34 +0300

    update style

--- react-website/src/components/HeroSection/index.js
+++ react-website/src/components/HeroSection/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { Button } from 'react-scroll';
+import Video from '../../videos/video.mp4';
+import {
+	HeroContainer,
+	HeroBg,
+	VideoBg,
+	HeroContent,
+	HeroH1,
+	HeroP,
+	HeroBtnWrapper,
+	ArrowForward,
+	ArrowRight
+} from './HeroElemets';
 
 const HeroSection = () => {
+	const [ hover, setHover ] = useState(false);
+
+	const onHover = () => {
+		setHover(!hover);
+	};
+
 	return (
 		<HeroContainer>
 			<HeroBg>
 				<VideoBg autoPlay loop muted src={Video} type="video/mp4" />
 			</HeroBg>
+			<HeroContent>
+				<HeroH1>Virtual Site</HeroH1>
+				<HeroP>Sign Up for a new account today and receive your first task</HeroP>
+				<HeroBtnWrapper>
+					<Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+						Get started{hover ? <ArrowForward /> : <ArrowRight />}{' '}
+					</Button>
+				</HeroBtnWrapper>
+			</HeroContent>
 		</HeroContainer>
 	);
 };
