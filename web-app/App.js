a99aca7c87e7df7c823ba5c545d6bf4a8b6872c2
Sergey Ermakov <oino-dev@ya.ru>
2021-01-25 13:38:44 +0300

    react create

--- web-app/App.js
+++ web-app/App.js
@@ -1,27 +1,24 @@
-const express = requier('express')
-const config = requier('config')
-// const mongoose = require('mongoose')
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
+app.use('/api/auth', require('./routes/auth.routes'));
 
-const PORT = config.get('port') || 5000
-
+const PORT = config.get('port') || 5000;
 
 async function start() {
-  try {
-    await mongoose.connect(config.get('mongoUri'), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-
-    })
-    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
-
-  } catch (e) {
-    console.log('Server Error', e.message)
-    // process.exit(code: 1)
-  }
+	try {
+		await mongoose.connect(config.get('mongoUri'), {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		});
+		app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+	} catch (e) {
+		console.log('Server Error', e.message);
+		// process.exit((code: 1));
+	}
 }
 
-start()
-
+start();
---
+++ web-app/routes/auth.routes.js
@@ -0,0 +1,88 @@
+const { Router } = require('express');
+const User = require('../models/user');
+const router = Router();
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
+// /api/auth/register
+router.post(
+	'/register',
+	[
+		check('email', 'некорректный email').isEmail(),
+		check('password', 'Минимальная длина пароля 8 символов').isLength({ min: 8 })
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					errors: errors.array(),
+					message: 'Некоректные данные при регистрации'
+				});
+			}
+
+			const { email, password } = req.body;
+
+			const candidate = await User.findOne({ email });
+			if (candidate) {
+				return res.status(400).json({ message: 'User already registred' });
+			}
+
+			const hashedPassword = await bcrypt.hash(password, 12);
+			const user = new User({ enail, password: hashedPassword });
+
+			await user.save();
+
+			res.status(201).json({ message: 'User registred' });
+		} catch (e) {
+			res.status(500).json({ message: 'Somethink wrong. Try again' });
+		}
+	}
+);
+
+// /api/auth/login
+router.post(
+	'/login',
+	[
+		check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+		check('password', 'Введите пароль').exist()
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					errors: errors.array(),
+					message: 'Некоректные данные при взоде в систему'
+				});
+			}
+			const { email, password } = req.body;
+			const user = await User.findOne({ email });
+			if (!user) {
+				return res.status(400).json({ message: 'пользователь не найден' });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res.status(400).json({ message: 'Неверный логин или пароль' });
+			}
+
+			const token = jwt.sign(
+				{
+					userId: user.id
+				},
+				config.get('jwtSecret'),
+				{ expiresIn: '1h' }
+			);
+
+			res.json({ token, userId: user.id });
+		} catch (e) {
+			res.status(500).json({ message: 'Somethink wrong. Try again' });
+		}
+	}
+);
+
+module.exports = router;
